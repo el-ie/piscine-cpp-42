a1bce0c575dd3488c231bea673e74c3f45f219b1
Elie AMAR <eamar@e1r10p13.clusters.42paris.fr>
2022-12-24 21:05:56 +0100

    ex02

---
+++ DAY01/ex02/main.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+
+int	main(void) {
+	
+	std::string	str = "HI THIS IS BRAIN";
+
+	std::string*	stringPTR = &str;
+
+	std::string&	stringREF = str;
+	
+	std::cout << "Adresse = " << &str << std::endl;
+	std::cout << "Adresse = " << stringPTR << std::endl;
+	std::cout << "Adresse = " << &stringREF << std::endl;
+
+	std::cout << "str = " << str << std::endl;
+	std::cout << "*stringPTR = " << *stringPTR << std::endl;
+	std::cout << "stringREF = " << stringREF << std::endl;
+}
