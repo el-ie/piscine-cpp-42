a6f3fc27e1217dcb99017c7746586148fe0864bd
bob <test@test.com>
2023-01-27 11:55:17 -0500

    idea generator working

--- DAY04/ex01/Brain.cpp
+++ DAY04/ex01/Brain.cpp
@@ -1,4 +1,5 @@
 #include "Brain.hpp"
+#include <cstdlib> //ok?
 
 Brain::Brain() 
 {
@@ -8,9 +9,21 @@ Brain::Brain()
 
 void	Brain::createIdeas(void) {
 
+	std::string creature[10] = {"A penguin ", "A crow ", "A bear ", "An alligator ", "Cartman ", "Rick Sanchez ", "Morty ", "Donald duck ", "Donald Trump ", "Napoleon "};
+	std::string verb[10] = {"eating ", "seeing ", "befriending ", "speaking to ", "asking help from ", "questionning ", "attacking ", "hitting ", "escaping ", "loving "};
+	std::string item[10] = {"an icecream.", "logan paul.", "emmanuel macron.", "the entire cosmic realm.", "a crazy fox.", "a genius hamster.", "an evil plankton.", "morgan freeman.", "Neil amstrong.", "reality."};
+
+	srand((unsigned) time(NULL));
+	int	r1;
+	int	r2;
+	int	r3;
+
 	for (int i = 0; i < 100; i++) {
-		ideas[i] = (i % 26) + 65;
-		i++;
+		r1 = rand() % 10;	
+		r2 = rand() % 10;	
+		r3 = rand() % 10;
+		std::string str;
+		ideas[i] = str.append(creature[r1]).append(verb[r2]).append(item[r3]);
 	}
 }
 
--- DAY04/ex01/main.cpp
+++ DAY04/ex01/main.cpp
@@ -11,37 +11,40 @@ int	main(void)
 {
 	Brain* tete = new Brain;
 
-	std::cout << "idea = [" << tete->ideas[0] << "] <--" << std::endl;
+	//std::cout << "idea = [" << tete->ideas[0] << "] <--" << std::endl;
+
+	for (int i = 0; i < 10; i++)
+		std::cout << "[" << tete->ideas[i] << "]"  << std::endl;
 
 	//printf("%s", tete->ideas[0].c_str());
 }
 
 /*
-int	main(void)
-{
-	const Animal* egg = new Animal();
-	const Animal* milou = new Dog();
-	const Animal* garfield = new Cat();
-	std::cout << "milou type = " << milou->getType() << " " << std::endl;
-	std::cout << "garfield type = " << garfield->getType() << " " << std::endl;
-	garfield->makeSound();
-	milou->makeSound();
-	egg->makeSound();
+   int	main(void)
+   {
+   const Animal* egg = new Animal();
+   const Animal* milou = new Dog();
+   const Animal* garfield = new Cat();
+   std::cout << "milou type = " << milou->getType() << " " << std::endl;
+   std::cout << "garfield type = " << garfield->getType() << " " << std::endl;
+   garfield->makeSound();
+   milou->makeSound();
+   egg->makeSound();
 
-	std::cout << "------------------------------------------------" << std::endl;
+   std::cout << "------------------------------------------------" << std::endl;
 
-	const WrongAnimal* pikachu = new WrongAnimal();
-	const WrongAnimal* batman = new WrongCat();
-	std::cout << "WrongCat type = " <<  batman->getType() << " " << std::endl;
-	batman->makeSound();
-	pikachu->makeSound();
+   const WrongAnimal* pikachu = new WrongAnimal();
+   const WrongAnimal* batman = new WrongCat();
+   std::cout << "WrongCat type = " <<  batman->getType() << " " << std::endl;
+   batman->makeSound();
+   pikachu->makeSound();
 
-	delete egg;
-	delete milou;
-	delete garfield;
-	delete pikachu;
-	delete batman;
+   delete egg;
+   delete milou;
+   delete garfield;
+   delete pikachu;
+   delete batman;
 
-	return (0);
-}
-*/
+   return (0);
+   }
+ */
