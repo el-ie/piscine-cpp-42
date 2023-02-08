855219d49922db077a7d26814811f48a66803910
bob <test@test.com>
2023-02-08 02:51:14 -0500

    Dog working properly

--- DAY04/ex01/Dog.cpp
+++ DAY04/ex01/Dog.cpp
@@ -1,36 +1,48 @@
 #include "Dog.hpp"
 #include "Animal.hpp"
+#include "Brain.hpp"
+
+#include <typeinfo> // remove
 
 Dog::Dog() : Animal()
 {
 	type = "dog";
 	std::cout << "D[CONSTRUCTOR DEF] Dog is created." << std::endl;
+	head = new Brain;
 }
 
 Dog::Dog(const std::string &type) : Animal(type)
 {
 	std::cout << "D[CONSTRUCTOR STR] Dog is created." << std::endl;
+	head = new Brain;
 }
 
 Dog::Dog(const Dog &other) : Animal()
 {
+	std::cout << "D[CONSTRUCTOR CPY] Dog is created." << std::endl;
+	head = new Brain;
 	*this = other;
-
-	std::cout << "D[CONSTRUCTOR CPY] Dog " << type << " is created." << std::endl;
-}
-
-Dog::~Dog()
-{
-	std::cout << "D[DESCTRUCTOR] Dog is destroyed."<< std::endl;
 }
 
 Dog&	Dog::operator=(const Dog &other)
 {
-	this->type = other.type;
+	std::cout << "D[=] Dog assignated." << std::endl;
 
-	std::cout << "D[ASSIGNATION] Dog assignated." << std::endl;
+	type = other.type;
+	*head = *(other.head);
+
 	return *this;
 }
+
+Dog::~Dog()
+{
+	delete head;
+	std::cout << "D[DESCTRUCTOR] Dog is destroyed."<< std::endl;
+}
+
+std::string	Dog::get_brain_idea(int index) {
+	return (head->get_idea(index));
+	}
 
 void	Dog::makeSound(void) const {
 	std::cout << type << ": Wouf wouf im a fuging dog" << std::endl;
@@ -40,8 +52,3 @@ void	Dog::makeSound(void) const {
 std::string	Dog::getType(void) const {
 	return (type);
 }
-
-//void	Dog::set_()
-//void	Dog::get_()
-//void	Dog::()
-//void	Dog::_()
