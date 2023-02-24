372bc6e1088fcda97301331dbca3777fd9ec123e
Elie AMAR <eamar@e1r6p11.clusters.42paris.fr>
2023-02-24 19:54:58 +0100

    fin ex01 ?

--- DAY06/ex01/Data.hpp
+++ DAY06/ex01/Data.hpp
@@ -7,6 +7,7 @@ struct Data {
 
 	int		a;
 	double		b;
+	char		c;
 	std::string	str;
 };
 #endif
--- DAY06/ex01/main.cpp
+++ DAY06/ex01/main.cpp
@@ -6,22 +6,30 @@ int	main(void)
 	Data	obj;
 	Data *	Dptr = &obj;
 
+	std::cout << std::endl << "Data struct variable before serialization :" << std::endl << std::endl;
 	Dptr->a = 42;
 	Dptr->b = 42.42;
+	Dptr->c = 'A';
 	Dptr->str = "Hello";
 
-	std::cout << Dptr->a << std::endl;
-	std::cout << Dptr->b << std::endl;
-	std::cout << Dptr->str << std::endl;
+	std::cout << "a = " << Dptr->a << std::endl;
+	std::cout << "b = " << Dptr->b << std::endl;
+	std::cout << "c = " << Dptr->c << std::endl;
+	std::cout << "str = " << Dptr->str << std::endl;
 
 	uintptr_t tmp = Serializer::serialize(Dptr);
 
 	Data * newPtr = Serializer::deserialize(tmp);
 
-	std::cout << newPtr->a << std::endl;
-	std::cout << newPtr->b << std::endl;
-	std::cout << newPtr->str << std::endl;
+	std::cout << std::endl << "----------------------------------" << std::endl;
+	std::cout << std::endl << "Data ptr recovered from deserialization :" << std::endl << std::endl;
 
+	std::cout << "a = " << newPtr->a << std::endl;
+	std::cout << "b = " << newPtr->b << std::endl;
+	std::cout << "c = " << newPtr->c << std::endl;
+	std::cout << "str = " << newPtr->str << std::endl;
+
+	std::cout << std::endl;
 	
 	return 0;
 }
