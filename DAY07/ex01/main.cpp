fba11c5dee6c6350e5a5aeb34f1be7c63927af49
Elie AMAR <eamar@e1r6p3.clusters.42paris.fr>
2023-02-25 22:28:34 +0100

    to upper

--- DAY07/ex01/main.cpp
+++ DAY07/ex01/main.cpp
@@ -1,6 +1,17 @@
 #include <string>
 #include <iostream>
 
+void	display_string_up(std::string str) {
+	for (int i = 0; i < (int)str.size(); i++)
+		std::cout << (char)std::toupper(str[i]);
+	std::cout << " ";
+}
+
+template <typename T>
+void	call_display_up(const T& element) {
+		display_string_up(element);
+}
+
 template <typename T>
 void	display(const T& element) {
 	std::cout << element << " ";
@@ -26,7 +37,7 @@ void	iter(T* array, size_t size, void (*fct)(const T&)) {
 int	main(void)
 {
 
-	//tests avec des int
+	/////////////////tests avec des int
 	std::cout << std::endl << "------------Tests avec des INT----------------" << std::endl << std::endl;
 
 	//creation tableau
@@ -42,7 +53,7 @@ int	main(void)
 
 
 
-	//tests avec des doubles
+	/////////////////tests avec des doubles
 	std::cout << "------------Tests avec des DOUBLES----------------" << std::endl << std::endl;
 	
 	//creation tableau
@@ -55,4 +66,23 @@ int	main(void)
 	//affichage double des valeurs
 	std::cout << "fonction iter avec display double :" << std::endl;
 	iter<double>(double_tab, 4, &display_double);
+
+	
+	/////////////////tests avec des std::string
+
+	//creation du tableau
+	std::string liste[5] = {"monsieur", "banane", "mange", "des", "pommes"};
+
+	//affichage des valeurs
+	std::cout << "fonction iter avec display :" << std::endl;
+	iter<std::string>(liste, 5, &display);
+
+	//affichage double des valeurs
+	std::cout << "fonction iter avec to upper :" << std::endl;
+	iter<std::string>(liste, 5, &call_display_up);
 }
+
+
+
+
+
