9ea3d5328eb88865d91802ea53d1c636ac1698aa
Elie AMAR <eamar@e2r11p5.clusters.42paris.fr>
2023-02-28 17:00:55 +0100

    good avec les containers vector list et deque

--- DAY08/ex00/easyfind.hpp
+++ DAY08/ex00/easyfind.hpp
@@ -9,43 +9,33 @@
 #include <algorithm>
 
 template <typename T>
-typename T::iterator	easyfind(const T& contain, const int nb) {
+typename T::iterator	easyfind(T& contain, const int nb) {
 	
+	std::cout << std::endl;
 	typename T::iterator	result;
-	//typename T::iterator	result = contain.begin();
 
 
 	result = std::find(contain.begin(), contain.end(), nb);
 
 	if (result == contain.end()) {
 		std::cout << nb << " not found in container" << std::endl;	
-		return (NULL);
+		return (result);
 	}
-	
+	std::cout << nb << " found in container." << std::endl;
 	return (result);
 }
 
 template <typename T>
-void	display_values(const T& contain) {
+void	display_values(T& contain) {
 	
 	std::cout << std::endl;
 
-	//typename T::iterator it = contain.begin();
+	typename T::iterator it = contain.begin();
 
-	//typename T::iterator it = typename T::iterator(contain.begin());
-
-	typename T::iterator it;
-
-	for (it = contain.begin(); it != contain.end(); ++it)
-		std::cout << *it << " ";
-
-/*
 	while (it != contain.end()) {
 		std::cout << *it << " ";
 		++it;
 	}
-*/
-	(void)contain;
 
 	std::cout << std::endl;
 }
--- DAY08/ex00/main.cpp
+++ DAY08/ex00/main.cpp
@@ -5,40 +5,114 @@
 #include <iostream>
 
 // CHCKER
-#include <vector>
 #include <iterator>
 #include <algorithm>
+#include <vector>
+#include <list>
+#include <deque>
+
+
+//for (std::vector<int>::iterator it = liste.begin(); it != liste.end(); ++it)
+//	std::cout << *it << " ";
 
 int	main(void)
 {
-	
-	std::cout << std::endl << "Utilisation de la fonction easyfind avec differents containers:" << std::endl;
 
-	std::cout << "Avec le conteneur VECTOR : " << std::endl << std::endl;
+	std::cout << std::endl << "---------------------------------------------------" << std::endl;
+	std::cout << std::endl << "Utilisation de la fonction easyfind avec differents containers :" << std::endl;
+
+		////	CONTENEUR VECTOR	////
+
+	std::cout << std::endl << "---------------------------------------------------" << std::endl;
+	std::cout << std::endl << "Avec le conteneur VECTOR : " << std::endl;
 
 	//creation du vector
 	std::vector<int>	liste;
 
 	//assignation des valeurs
-	for (int i = 42; i < 1000; i *= 2)
+	for (int i = 42; i < 42000; i *= 2)
 		liste.push_back(i);
 
-	//creation de l'iterator pour afficher la list
-	std::vector<int>::iterator it;
-
-//	for (it = liste.begin(); it != liste.end(); ++it)
-//		std::cout << *it << " ";
-
+	//affichage des valeurs
 	display_values(liste);
+		
+	//creation de l'iterateur pour stocker le resultat
+	std::vector<int>::iterator result;
 
-	//easyfind(liste, 5);
-	
+	// test pour trouver un nombre present dans la liste
+	std::cout << std::endl << "Test avec un nombre present dans la liste :" << std::endl;
+	result = easyfind(liste, 672);
+	std::cout << "returned iterator = " << *result << std::endl;
+
+	std::cout << std::endl << "Test avec un nombre absent de la liste :" << std::endl;
+	result = easyfind(liste, 12);
+
+	if (result == liste.end())
+		std::cout << "Le retour de easyfind correspond bien a END." << std::endl;
+
 	std::cout << std::endl;
 
-	/*
-	if (it != liste.end())
-			std::cout << "element trouve" << std::endl;
-	else
-		std::cout << "Element absent" << std::endl;
-		*/
+		////	CONTENEUR LIST	////
+
+	std::cout << std::endl << "---------------------------------------------------" << std::endl;
+	std::cout << std::endl << "Avec le conteneur LIST : " << std::endl;
+
+	//creation du list
+	std::list<int>	liste2;
+
+	//assignation des valeurs
+	for (int i = 42; i < 42000; i *= 2)
+		liste2.push_back(i);
+
+	//affichage des valeurs
+	display_values(liste2);
+		
+	//creation de l'iterateur pour stocker le resultat
+	std::list<int>::iterator result2;
+
+	// test pour trouver un nombre present dans la liste2
+	std::cout << std::endl << "Test avec un nombre present dans la liste2 :" << std::endl;
+	result2 = easyfind(liste2, 1344);
+	std::cout << "returned iterator = " << *result2 << std::endl;
+
+	std::cout << std::endl << "Test avec un nombre absent de la liste2 :" << std::endl;
+	result2 = easyfind(liste2, 13);
+
+	if (result2 == liste2.end())
+		std::cout << "Le retour de easyfind correspond bien a END." << std::endl;
+
+	std::cout << std::endl;
+
+
+		////	CONTENEUR DEQUE	////
+
+	std::cout << std::endl << "---------------------------------------------------" << std::endl;
+	std::cout << std::endl << "Avec le conteneur DEQUE : " << std::endl;
+
+	//creation du deque
+	std::deque<int>	liste3;
+
+	//assignation des valeurs
+	for (int i = 42; i < 42000; i *= 2)
+		liste3.push_back(i);
+
+	//affichage des valeurs
+	display_values(liste3);
+		
+	//creation de l'iterateur pour stocker le resultat
+	std::deque<int>::iterator result3;
+
+	// test pour trouver un nombre present dans la liste3
+	std::cout << std::endl << "Test avec un nombre present dans la liste3 :" << std::endl;
+	result3 = easyfind(liste3, 2688);
+	std::cout << "returned iterator = " << *result3 << std::endl;
+
+	std::cout << std::endl << "Test avec un nombre absent de la liste3 :" << std::endl;
+	result3 = easyfind(liste3, 14);
+
+	if (result3 == liste3.end())
+		std::cout << "Le retour de easyfind correspond bien a END." << std::endl;
+
+	std::cout << std::endl;
+	return 0;
 }
